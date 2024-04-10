dedcea89dbb61593a4495ee957d8edb181797eb1
Laércio de Sousa <lbsousajr@gmail.com>
2024-04-10 21:58:06 +0000

    Fix typo in setup.sh.

--- setup.sh
+++ setup.sh
@@ -56,7 +56,7 @@ do
 done
 
 # Make TeX Live fonts available for LilyPond
-TEXLIVE_FONTS_CONF=$(find /usr/local/texlive texlive-fontconfig.conf)
+TEXLIVE_FONTS_CONF=$(find /usr/local/texlive -name texlive-fontconfig.conf)
 ln -s ${TEXLIVE_FONTS_CONF} /opt/lilypond-${LILYPOND_VERSION}/etc/fonts/conf.d/99-texlive.conf
 
 # Clean up
